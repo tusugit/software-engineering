ddd8a38bfaa34cac8638f6267a6e9f25143fb2d4
tusugit <94559152+tusugit@users.noreply.github.com>
2022-06-09 11:11:56 +0800

    Add files via upload
    实验一-最大子数组和

---
+++ test1.cpp
@@ -0,0 +1,27 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+
+#include <iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+	ifstream cin("input.txt"); //把输入流定位到aaa.txt文件
+	int nums[1000] = { 0 }; //数组
+	int m, n;//行列
+	cin >> m >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> nums[i];
+	}
+	cout << "数组的行为" << m << "数组的列为" << n << endl;;
+	cout << "数组的数据如下" << endl;
+		for (int i = 0; i < n; i++) {
+			cout << nums[i] << "    ";
+		}
+	int pre = 0, maxAns = nums[0];
+	for (const auto& x : nums) {
+		pre = max(pre + x, x);
+		maxAns = max(maxAns, pre);
+	}
+	cout << endl << "最大子数组的和为" << maxAns;
+}
